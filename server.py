c47b38d3ba8133984872b6435866d320d858c23f
dieHackerin <maytie@gmail.com>
2014-05-23 16:27:34 +0200

    server and client are now classes

--- server.py
+++ server.py
@@ -5,72 +5,95 @@ from thread import *
 import AESCipher
 import uuid
 import hashlib
-
-HOST = ''   # Symbolic name meaning all available interfaces
-PORT = 8888  # Arbitrary non-privileged port
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Socket created'
-
-#Bind socket to local host and port
-try:
-    s.bind((HOST, PORT))
-except socket.error, msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
-
-print 'Socket bind complete'
-
-#Start listening on socket
-s.listen(10)
-print 'Socket now listening'
+from Crypto.PublicKey import RSA
 
 
-#Function for handling connections. This will be used to create threads
-def clientthread(conn):
-    #Sending message to connected client
-    #conn.send('Welcome to the server. Type something and hit enter\n')  # send only takes string
+class ServerInstance:
 
-    #infinite loop so that function do not terminate and thread do not end.
-    while True:
+    def __init__(self):
+        self.username = ""
+        self.pwd = ""
+        self.session_key = ""
 
-        #Receiving from client
-        data = conn.recv(1024)
-        key = data
-        aes = AESCipher.AESCipher(key)
+    #Function for handling connections. This will be used to create threads
+    def client_thread(self, conn):
+        #Sending message to connected client
+        #conn.send('Welcome to the server. Type something and hit enter\n')  # send only takes string
 
-        user = aes.decrypt(conn.recv(1024))
-        pwd = aes.decrypt(conn.recv(1024))
-        print user
-        #antwort
-        chalange = str(uuid.uuid4())
-        cyphertext = aes.encrypt(chalange)
+        #infinite loop so that function do not terminate and thread do not end.
+        while True:
+
+            #Receiving from client
+            data = conn.recv(1024)
+            self.session_key = AESCipher.AESCipher(data)
+
+            self.username = self.session_key.decrypt(conn.recv(1024))
+            self.pwd = self.session_key.decrypt(conn.recv(1024))
+
+            # answer
+            challenge = str(uuid.uuid4())
+            cypher_text = self.session_key.encrypt(challenge)
+
+            if not data:
+                break
+
+            conn.send(cypher_text)
+            data = self.session_key.decrypt(conn.recv(1024))
+            if data == hashlib.sha1(challenge+self.username).digest():
+                temp_pwd = self.session_key.decrypt(conn.recv(1024))
+                temp_rand = hashlib.md5(str(uuid.uuid4())).hexdigest()[:5]
+                #TODO add database parts here
+                conn.send(self.session_key.encrypt(temp_rand))
+
+                print("fuck")
+            else:
+                conn.send(self.session_key.encrypt("__ERROR"))
+        #came out of loop
+        conn.close()
 
 
-        if not data:
-            break
+class Server:
+    def __init__(self):
+        try:
+            with open('id_rsa', 'r') as key_file:
+                key = key_file.read()
+            self.private = RSA.importKey(key)
+        except IOError:
+            print "Unable to read key!"
 
-        conn.send(cyphertext)
-        data = aes.decrypt(conn.recv(1024))
-        if data == hashlib.sha1(chalange+user).digest():
-            temp_pwd = aes.decrypt(conn.recv(1024))
-            temp_rand = hashlib.md5(str(uuid.uuid4())).digest()
-            #TODO add database parts here
-            conn.send(aes.encrypt(temp_rand))
+        self.host = ''   # Symbolic name meaning all available interfaces
+        self.port = 8888  # Arbitrary non-privileged port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print 'Socket created'
 
-            print("fuck")
-        else:
-             conn.send(aes.encrypt("__ERROR"))
-    #came out of loop
-    conn.close()
+    def open_socket(self):
+        #Bind socket to local host and port
+        try:
+            self.socket.bind((self.host, self.port))
+        except socket.error, msg:
+            print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            sys.exit()
 
-#now keep talking with the client
-while 1:
-    #wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+        print 'Socket bind complete'
 
-    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    start_new_thread(clientthread, (conn,))
+        #Start listening on socket
+        self.socket.listen(10)
+        print 'Socket now listening'
 
-s.close()
+    def handle_clients(self):
+        #now keep talking with the client
+        while True:
+            #wait to accept a connection - blocking call
+            conn, address = self.socket.accept()
+            print 'Connected with ' + address[0] + ':' + str(address[1])
+
+            #start new thread takes 1st argument as a function name to be run,
+            # second is the tuple of arguments to the function.
+            server_instance = ServerInstance()
+            start_new_thread(server_instance.client_thread, (conn,))
+
+        ServerInstance.socket.close()
+
+server = Server()
+server.open_socket()
+server.handle_clients()
