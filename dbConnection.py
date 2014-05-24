f31ca4b76768cffc4a18f83c31be1656a463e2cf
dieHackerin <maytie@gmail.com>
2014-05-24 12:55:00 +0200

    renames

--- dbConnection.py
+++ dbConnection.py
@@ -1,24 +1,29 @@
-import MySQLdb, time
+import MySQLdb
+import time
 
-db = MySQLdb.connect(host="localhost",user="root",passwd="mytienmagsit",db="database")
+db = MySQLdb.connect(host="localhost", user="root", passwd="mytienmagsit", db="database")
 
 cur = db.cursor()
 cur.execute()
 
-def GetUserPw(username):
+
+def get_user_pw(username):
     userpw = cur.execute("SELECT password FROM user where Username ="+username+";")
     return userpw
 
-def InsertSession(username,hash):
-    cur.execute("INSERT INTO session(Timestamp,hash) VALUES ("+time.time()+","+hash+") WHERE Username='"+username+"';")
 
-def Validate(username,hash):
-    dbTimestamp = cur.execute("SELECT Timestamp FROM Session WHERE Username='"+username+"'AND hash='"+hash+"';")
-    if dbTimestamp - time.time() < 30:
+def insert_session(username, hash_val):
+    cur.execute("INSERT INTO session(Timestamp,hash) VALUES ("+time.time()+","+hash_val+") WHERE Username='"+username+"';")
+
+
+def validate(username, hash_val):
+    db_timestamp = cur.execute("SELECT Timestamp FROM Session WHERE Username='"+username+"'AND hash='"+hash_val+"';")
+    if db_timestamp - time.time() < 30:
         return True
     else:
         return False
 
-def IsValid(username,hash):
-    isValid = cur.execute("SELECT valid FROM Session WHERE Username='"+username+"'AND hash='"+hash+"';")
+
+def is_valid(username, hash_val):
+    isValid = cur.execute("SELECT valid FROM Session WHERE Username='"+username+"'AND hash='"+hash_val+"';")
     return isValid
