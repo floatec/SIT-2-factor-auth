8b7762b93f10c7c0413ef8c1c481ac4046c9adf3
silv <bw.gosu@web.de>
2014-05-28 17:07:06 +0200

    methods tested
    should work ;)

--- dbConnection.py
+++ dbConnection.py
@@ -1,24 +1,37 @@
 import MySQLdb, time
 
+
 db = MySQLdb.connect(host="localhost", user="root", passwd="mytienmagsit", db="database")
 
 cur = db.cursor()
-cur.execute()
+
 
 def GetUserPw(username):
-    userpw = cur.execute("SELECT password FROM user where Username ='%s';", (username))
-    return userpw
+    cur.execute("SELECT password FROM user WHERE Username =%s;", (username,))
+    return cur.fetchone()
 
 def InsertSession(username, hash):
-    cur.execute("INSERT INTO session(Timestamp,hash) VALUES ("+time.time()+",%s) WHERE Username='%s';", (hash, username))
+    print str(time.time())
+    cur.execute("INSERT INTO session(actualtime,hash,username) VALUES (%s,%s,%s)", (float(time.time()),hash, username))
+    db.commit()
 
 def Validate(username, hash):
-    dbTimestamp = cur.execute("SELECT Timestamp FROM Session WHERE Username='%s' AND hash='%s';", (username, hash))
-    if dbTimestamp - time.time() < 30:
+    cur.execute("SELECT actualtime FROM Session WHERE Username=%s AND hash=%s;", (username, hash))
+    dbTimestamp = cur.fetchone()
+    print dbTimestamp[0]
+    print time.time()
+    diff = (time.time() - dbTimestamp[0])
+    print diff
+    valid=False
+    if float(diff) < 30.0 and float(dbTimestamp[0]) > 0.0:
+        cur.execute("UPDATE session SET valid=1 WHERE Username=%s AND hash=%s AND actualtime=%s",(username,hash,dbTimestamp[0]))
         return True
     else:
         return False
 
 def IsValid(username, hash):
-    isValid = cur.execute("SELECT valid FROM Session WHERE Username='%s'AND hash='%s';", (username, hash))
-    return isValid
+    isValid = cur.execute("SELECT valid FROM Session WHERE Username=%s AND hash=%s;", (username, hash))
+    if isValid == 1:
+        return True
+    else:
+        return False
