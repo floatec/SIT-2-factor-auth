0d5e24d9d178d1b8156885a67299cf9b7e525f31
dieHackerin <maytie@gmail.com>
2014-05-29 14:16:32 +0200

    PEP conformity

--- dbConnection.py
+++ dbConnection.py
@@ -1,37 +1,42 @@
-import MySQLdb, time
-
+import MySQLdb
+import time
 
 db = MySQLdb.connect(host="localhost", user="root", passwd="mytienmagsit", db="database")
 
 cur = db.cursor()
 
 
-def GetUserPw(username):
+def get_user_pw(username):
     cur.execute("SELECT password FROM user WHERE Username =%s;", (username,))
     return cur.fetchone()
 
-def InsertSession(username, hash):
+
+def insert_session(username, hash_val):
     print str(time.time())
-    cur.execute("INSERT INTO session(actualtime,hash,username) VALUES (%s,%s,%s)", (float(time.time()),hash, username))
+    cur.execute("INSERT INTO session(actualtime,hash,username) VALUES (%s,%s,%s)",
+                (float(time.time()), hash_val, username))
     db.commit()
 
-def Validate(username, hash):
-    cur.execute("SELECT actualtime FROM Session WHERE Username=%s AND hash=%s;", (username, hash))
-    dbTimestamp = cur.fetchone()
-    print dbTimestamp[0]
+
+def validate(username, hash_val):
+    cur.execute("SELECT actualtime FROM Session WHERE Username=%s AND hash=%s;", (username, hash_val))
+    db_timestamp = cur.fetchone()
+    print db_timestamp[0]
     print time.time()
-    diff = (time.time() - dbTimestamp[0])
+    diff = (time.time() - db_timestamp[0])
     print diff
-    valid=False
-    if float(diff) < 30.0 and float(dbTimestamp[0]) > 0.0:
-        cur.execute("UPDATE session SET valid=1 WHERE Username=%s AND hash=%s AND actualtime=%s",(username,hash,dbTimestamp[0]))
+    valid = False
+    if float(diff) < 30.0 and float(db_timestamp[0]) > 0.0:
+        cur.execute("UPDATE session SET valid=1 WHERE Username=%s AND hash=%s AND actualtime=%s",
+                    (username, hash_val, db_timestamp[0]))
         return True
     else:
         return False
 
-def IsValid(username, hash):
-    isValid = cur.execute("SELECT valid FROM Session WHERE Username=%s AND hash=%s;", (username, hash))
-    if isValid == 1:
+
+def is_valid(username, hash_val):
+    valid = cur.execute("SELECT valid FROM Session WHERE Username=%s AND hash=%s;", (username, hash_val))
+    if valid == 1:
         return True
     else:
         return False
