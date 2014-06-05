0f3a25b8f127750ad4f6898119e55b29fe97e5a1
dieHackerin <maytie@gmail.com>
2014-06-05 16:24:25 +0200

    current database

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2014 at 03:15 PM
+-- Generation Time: Jun 04, 2014 at 02:29 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -27,11 +27,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `session` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `Username` varchar(40) NOT NULL,
-  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `actualtime` double NOT NULL,
   `hash` text NOT NULL,
-  PRIMARY KEY (`Username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `valid` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -42,6 +44,7 @@ CREATE TABLE IF NOT EXISTS `session` (
 CREATE TABLE IF NOT EXISTS `user` (
   `Username` varchar(40) NOT NULL,
   `password` text NOT NULL,
+  `salt` varchar(40) NOT NULL,
   PRIMARY KEY (`Username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -49,8 +52,9 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`Username`, `password`) VALUES
-('Alice', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
+INSERT INTO `user` (`Username`, `password`, `salt`) VALUES
+('Alice', '0489f9fafffa5edc706c3041ea7e96120e90fb9f', 'bdb534d88ee9f2f49342860648b9d1fc40f7f847'),
+('Bob', '76bf7c094b5eaae301586cae07b2eb7e2a6ca73d', '6baa5061cd4082e2bd71b9ed9598baa846b525da');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
